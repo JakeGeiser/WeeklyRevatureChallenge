34c1b0bb24e2fc774d08a341712ebfcd6b0fe7e5
Jake Geiser <geiser.jt@gmail.com>
2022-05-11 16:22:20 -0400

    Initialize repo

---
+++ WeeklyRevatureChallenge/src/main/java/weeklychallange/week1/Week1.java
@@ -0,0 +1,17 @@
+package weeklychallange.week1;
+
+public class Week1 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public int[] magicIndex(int[] sortedArray) {
+		
+		
+		
+		
+		return sortedArray;
+		
+	}
+}
